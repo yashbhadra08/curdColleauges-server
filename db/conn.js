da3622bfbf75098d258283a899e011884d34a1bb
yashB <yashbhadra08@gmail.com>
2023-05-10 15:05:06 +0530

    Initial commit

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,10 @@
-const mongoose = require("mongoose");
-const DB = process.env.DB;
+mongoose = require("mongoose");
+const DB = `mongodb+srv://bhaikigaming4:Fakeidemily12@cluster0.dktcedx.mongodb.net/mernstack?retryWrites=true&w=majority`;
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => console.log("connection started")).catch((error) => console.log(error.message));
+  })
+  .then(() => console.log("connection started"))
+  .catch((error) => console.log(error.message));
