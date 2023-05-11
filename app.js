e6d57be5e2340668ec42b71c31cef1785d2e18d2
yashB <yashbhadra08@gmail.com>
2023-05-11 12:25:14 +0530

    Changes

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 require("./db/conn");
 const cors = require("cors");
-const router = require("./routes/router")
+const router = require("./routes/router");
 
-const port = 8003;
+const port = process.env.port || 8003;
 
 app.use(cors());
 app.use(express.json());
